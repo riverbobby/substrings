4f27249ca2f9d88e827165527cc9c0231ee35878
Justin Townley <justin.d.townley@gmail.com>
2022-08-20 10:56:34 -0700

    Finish challenge

--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,18 @@
 require 'pry-byebug'
 
 def substrings(string, dictionary)
-	
+	result = Hash.new(0)
+
+	dictionary.map do |substring|
+		string.downcase.scan(substring) { |match| result[match] += 1 }
+	end
+
+	return result
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 p substrings("below", dictionary)
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
