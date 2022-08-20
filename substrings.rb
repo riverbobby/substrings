83096c5b4fb45ed7e28421d44331b9f925088e23
Justin Townley <justin.d.townley@gmail.com>
2022-08-20 10:26:03 -0700

    Add basic file and structure to program

---
+++ substrings.rb
@@ -0,0 +1,10 @@
+require 'pry-byebug'
+
+def substrings(string, dictionary)
+	
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substrings("below", dictionary)
