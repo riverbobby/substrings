a6093fe3e79ae9ab78ff39868de9349f8fe55ff8
Justin Townley <justin.d.townley@gmail.com>
2022-10-13 09:01:03 -0700

    Run rubocop autocorrect

--- substrings.rb
+++ substrings.rb
@@ -1,18 +1,17 @@
 require 'pry-byebug'
 
 def substrings(string, dictionary)
-	result = Hash.new(0)
+  result = Hash.new(0)
 
-	dictionary.map do |substring|
-		string.downcase.scan(substring) { |match| result[match] += 1 }
-	end
+  dictionary.map do |substring|
+    string.downcase.scan(substring) { |match| result[match] += 1 }
+  end
 
-	return result
+  result
 end
 
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-p substrings("below", dictionary)
+p substrings('below', dictionary)
 
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
